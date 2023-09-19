940b2d23478b65425a0c45a3b9f90e801388d96a
Wang HZ <36946658+wang-hz@users.noreply.github.com>
2023-09-19 21:06:43 +0800

    add a solution for problem 2

---
+++ app/src/test/java/infra/ListNode.java
@@ -0,0 +1,43 @@
+package infra;
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public ListNode(int[] nums) {
+        ListNode head = new ListNode();
+        ListNode curr = head;
+        for (int num : nums) {
+            curr.next = new ListNode(num);
+            curr = curr.next;
+        }
+        if (head.next != null) {
+            this.val = head.next.val;
+            this.next = head.next.next;
+        }
+    }
+
+    public void print() {
+        System.out.print('[');
+        ListNode curr = this;
+        while (curr != null) {
+            System.out.print(curr.val);
+            curr = curr.next;
+            if (curr != null) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print(']');
+    }
+}
---
+++ app/src/test/java/problem2/AddTwoNumbersTest.java
@@ -0,0 +1,66 @@
+package problem2;
+
+import org.junit.jupiter.api.Test;
+
+import infra.ListNode;
+
+/*
+ * @lc app=leetcode id=2 lang=java
+ *
+ * [2] Add Two Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode();
+        ListNode p0 = res;
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        int carry = 0;
+        while (p1 != null || p2 != null) {
+            int sum = carry;
+            if (p1 == null) {
+                sum += p2.val;
+                p2 = p2.next;
+            } else if (p2 == null) {
+                sum += p1.val;
+                p1 = p1.next;
+            } else {
+                sum += p1.val + p2.val;
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+            if (sum >= 10) {
+                sum -= 10;
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+            p0.next = new ListNode(sum);
+            p0 = p0.next;
+        }
+        if (carry == 1) {
+            p0.next = new ListNode(carry);
+        }
+        return res.next;
+    }
+}
+// @lc code=end
+
+class AddTwoNumbersTest {
+    @Test
+    void case1() {
+        new Solution().addTwoNumbers(new ListNode(new int[]{2,4,3}), new ListNode(new int[]{5,6,4})).print();
+    }
+}
